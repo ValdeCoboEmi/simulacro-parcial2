2f1c6e667de4593be69627e3dc733e7f70ee3f49
ValdeCoboEmi <00208225@uca.edu.sv>
2025-06-04 12:01:31 -0600

    Cambios del ejercio de if

--- ejercicio19-if.cpp
+++ ejercicio19-if.cpp
@@ -9,24 +9,25 @@ int main(){
     //declaracion de variable
     int kWh;
     //Se le pide el consumo
-    cout << "Bienvenido a la calculadora de la tarifa de Luz Eléctrica" << endl;
+    cout << "Bienvenido a la calculadora de la tarifa de Luz Electrico" << endl;
     cout << "Por, favor ingrese el consumo de (kWh): ";
+    cin >> kWh;
 
     //Se comienza con la comparacion del kwh para comenzar
     if (kWh >= 0 && kWh < 100)
     {
         //Se imprime y el resultado se multiplica por el monto que va a la tarifa
-        cout << "Tiene un consumo de "<< kWh << " que es tarifa menor que equivale a ($0.25) y tiene que pagar: $" << (kWh*0.25) << endl;
+        cout << "Tiene un consumo de "<< kWh << " kWh que es tarifa menor que equivale a ($0.25) y tiene que pagar: $" << (kWh*0.25) << endl;
     }
     else if (kWh >= 100 && kWh <= 200)
     {
         //Se imprime y el resultado se multiplica por el monto que va a la tarifa
-        cout << "Tiene un consumo de "<< kWh << " que es tarifa medida que equivale a ($1) y tiene que pagar: $" << (kWh*1) << endl;
+        cout << "Tiene un consumo de "<< kWh << "  kWh que es tarifa medida que equivale a ($1) y tiene que pagar: $" << (kWh*1) << endl;
     }
     else if (kWh > 200)
     {
         //Se imprime y el resultado se multiplica por el monto que va a la tarifa
-        cout << "Tiene un consumo de "<< kWh << " que es tarifa alta que equivale a ($2) y tiene que pagar: $" << (kWh*2) << endl;
+        cout << "Tiene un consumo de "<< kWh << " kWh que es tarifa alta que equivale a ($2) y tiene que pagar: $" << (kWh*2) << endl;
     }
     else{
         cout << "Has ingresado incorrectamente el dato, vuelva a intentarlo" << endl;
